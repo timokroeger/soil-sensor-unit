8c8c08f8e0680c98915afef42751af947a4de5dd
Timo Kröger <timokroeger93@gmail.com>
2017-10-27 19:45:52 +0200

    Allow config updates over MODBUS.

--- src/modbus_data.cc
+++ src/modbus_data.cc
@@ -2,12 +2,21 @@
 
 #include "modbus_data.h"
 
+#include "config_storage.h"
+
 bool ModbusData::ReadRegister(uint16_t address, uint16_t *data_out) {
   (void)address;
   *data_out = raw_value_;
   return true;
 }
 
-bool ModbusData::WriteRegister(uint16_t, uint16_t) {
+bool ModbusData::WriteRegister(uint16_t address, uint16_t data) {
+  if (address < ConfigStorage::kMaxConfigIndex) {
+    ConfigStorage cs = ConfigStorage::Instance();
+    cs.Set(static_cast<ConfigStorage::ConfigIndex>(address), data);
+    cs.WriteConfigToFlash();
+    return true;
+  }
+
   return false;
 }
