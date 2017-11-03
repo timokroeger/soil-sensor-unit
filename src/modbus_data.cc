676a61ea456805355381ae475ba4a989e87382f8
Timo Kröger <timokroeger93@gmail.com>
2017-11-03 17:46:23 +0100

    Move config registers to address 128.
    Also allow to read the current config via MODBUS.

--- src/modbus_data.cc
+++ src/modbus_data.cc
@@ -4,16 +4,28 @@
 
 #include "config_storage.h"
 
+#define CONFIG_OFFSET 128u
+
 bool ModbusData::ReadRegister(uint16_t address, uint16_t *data_out) {
-  (void)address;
-  *data_out = raw_value_;
+  if (address == 0) {
+    *data_out = raw_value_;
+  } else if (address >= CONFIG_OFFSET &&
+             address < ConfigStorage::kMaxConfigIndex + CONFIG_OFFSET) {
+    *data_out = ConfigStorage::Instance().Get(
+        static_cast<ConfigStorage::ConfigIndex>(address - CONFIG_OFFSET));
+  } else {
+    return false;
+  }
+
   return true;
 }
 
 bool ModbusData::WriteRegister(uint16_t address, uint16_t data) {
-  if (address < ConfigStorage::kMaxConfigIndex) {
+  if (address >= CONFIG_OFFSET &&
+      address < ConfigStorage::kMaxConfigIndex + CONFIG_OFFSET) {
     ConfigStorage cs = ConfigStorage::Instance();
-    cs.Set(static_cast<ConfigStorage::ConfigIndex>(address), data);
+    cs.Set(static_cast<ConfigStorage::ConfigIndex>(address - CONFIG_OFFSET),
+           data);
     cs.WriteConfigToFlash();
     return true;
   }
