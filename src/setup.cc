ecf35c21d372d94dad2e0ced894655eec970a5cc
Timo Kröger <timokroeger93@gmail.com>
2019-03-22 12:14:30 +0100

    [stm32g0] Setup code for PLL

--- src/setup.cc
+++ src/setup.cc
@@ -2,6 +2,37 @@
 
 #include "setup.h"
 
+#include <cassert>
+
+#include "stm32g0xx_ll_bus.h"
+#include "stm32g0xx_ll_pwr.h"
+#include "stm32g0xx_ll_rcc.h"
+
+namespace {
+
+// Configures PLLQCLK to output 128MHz for TIM1.
+void SetupPll() {
+  // PLL must not be running when modifying registers.
+  assert(!LL_RCC_PLL_IsReady());
+
+  // 128MHz PLLQCLK is only possible with voltage scaling range 1.
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  assert(LL_PWR_GetRegulVoltageScaling() == LL_PWR_REGU_VOLTAGE_SCALE1);
+
+  LL_RCC_PLL_ConfigDomain_TIM1(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_1, 16,
+                               LL_RCC_PLLQ_DIV_2);
+  LL_RCC_PLL_EnableDomain_TIM1();
+
+  // Start PLL with IRC as source
+  LL_RCC_PLL_Enable();
+  while (!LL_RCC_PLL_IsReady())
+    ;
+
+  LL_RCC_SetTIMClockSource(LL_RCC_TIM1_CLKSOURCE_PLL);
+}
+
+}  // namespace
+
 void Setup() {
-  // TODO: Implement
+  SetupPll();
 }
